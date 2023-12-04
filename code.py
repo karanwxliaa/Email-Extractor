2cc54463f23aa29ebdfd86cc8fc68dfc07c540af
Jaskaran Singh Walia <95328038+karanwxliaa@users.noreply.github.com>
2023-12-05 00:29:22 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,17 @@
+import re
+
+def extract_emails(text):
+    # Define a regular expression pattern for matching emails
+    email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+    
+    # Use re.findall to find all matches in the text
+    emails = re.findall(email_pattern, text)
+    
+    return emails
+
+# Take input text from the user
+input_text = input("Enter a piece of text: ")
+
+# Extract and print the emails from the input text
+emails = extract_emails(input_text)
+print("Extracted emails:", emails)
